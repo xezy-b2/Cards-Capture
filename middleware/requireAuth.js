abefdb520924d5ea333930e2e241b3ec7dcba90c
xezy-b2 <xezy_4l@outlook.fr>
2026-07-19 11:38:56 +0200

    Delete middleware directory

--- middleware/requireAuth.js
+++
@@ -1,8 +0,0 @@
-function requireAuth(req, res, next) {
-  if (!req.session.user) {
-    return res.status(401).json({ error: 'Connecte-toi avec Discord pour faire ça.' });
-  }
-  next();
-}
-
-module.exports = { requireAuth };
